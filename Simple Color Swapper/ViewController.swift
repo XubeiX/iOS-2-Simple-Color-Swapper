1a4056e895e2942a6d72330bdb3d6103153b2a4f
Artur Ratajczak <artur.ratajczak1@gmail.com>
2018-10-06 02:47:05 -0700

    Initial Commit

---
+++ Simple Color Swapper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple Color Swapper
+//
+//  Created by Artur on 06/10/2018.
+//  Copyright © 2018 Artur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
